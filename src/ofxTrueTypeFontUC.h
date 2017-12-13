c2eea0fcfa2e09efab16c9b9d240e4e761124103
ChulseungYoo <csyoo@imlabworld.com>
2017-12-13 21:32:25 +0900

    ltr works. contexture char selecting done, yeh will be done later

--- src/ofxTrueTypeFontUC.h
+++ src/ofxTrueTypeFontUC.h
@@ -19,7 +19,7 @@ public:
   
   //set the default dpi for all typefaces.
   static void setGlobalDpi(int newDpi);
-  
+  bool isRTL = true;
   // 			-- default (without dpi), anti aliased, 96 dpi:
   bool load(string filename, int fontsize, bool bAntiAliased=true, bool makeContours=false, float simplifyAmt=0.3, int dpi=0);
   bool loadFont(string filename, int fontsize, bool bAntiAliased=true, bool makeContours=false, float simplifyAmt=0.3, int dpi=0);
@@ -29,6 +29,7 @@ public:
   void drawString(const string &str, float x, float y);
   void drawStringCenter(const string &str, float x, float y);
   void drawString(const wstring &str, float x, float y);
+  void drawString(const wstring & str, ofRectangle box);
   void drawStringOutline(const wstring &str, float x, float y);
   void drawStringCenter(const wstring &str, float x, float y);
   void drawStringCenter(const wstring &str, ofRectangle box);
